4294fabbe562fab0c45d6c363759f19207a825f7
viketvova <viketvova@gmail.com>
2021-04-13 21:31:56 +0200

    Add Navlinks and Header menu components

---
+++ src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function JuniorPlus() {
+    return (
+        <div>
+            {/*<HW12/>*/}
+            {/*<HW13/>*/}
+        </div>
+    )
+}
+
+export default JuniorPlus
+
+// сделайте по аналогии пустые страницы джун и джун+
+// туда будут добавляться следующие дз
